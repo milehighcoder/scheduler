2203f8dd1d762220772e11328cef5fd0ad465796
mykereel <mgmartnz@icloud.com>
2020-10-23 16:58:28 -0600

    changed type=button solution to event.preventDefault() instead

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-// const m = moment().format("[Today's date is] dddd, MMMM Do.");
+const m = moment().format("[Today's date is] dddd, MMMM Do.");
 
-//displays current day in viewport
-// $("#currentDay").html(m); //JS = document.getElementById('currentDay').innerHTML = m;
+// displays current day in viewport
+$("#currentDay").html(m); //JS = document.getElementById('currentDay').innerHTML = m;
 
 // 9:00 AM time block
-$("#button9").click(function () {
-  var input = $("#input9").val();
-  console.log(input)
-  localStorage.setItem("input", input);
+$("#button9AM").click(function () {
+  event.preventDefault();
+  textToSave = $("#input9AM").val();
+  console.log(textToSave);
+  localStorage.setItem("textToSave", textToSave);
 });
-
