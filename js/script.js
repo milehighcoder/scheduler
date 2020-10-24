c7e8cf51aa6f7bd067c3b1193a826ae3fe084685
mykereel <mgmartnz@icloud.com>
2020-10-24 10:08:11 -0600

    TA help version

--- js/script.js
+++ js/script.js
@@ -4,9 +4,22 @@ const m = moment().format("[Today's date is] dddd, MMMM Do.");
 $("#currentDay").html(m); //JS = document.getElementById('currentDay').innerHTML = m;
 
 // 9:00 AM time block
-$("#button9AM").click(function () {
+$("#nine").click(function () {
   event.preventDefault();
-  textToSave = $("#input9AM").val();
-  console.log(textToSave);
-  localStorage.setItem("textToSave", textToSave);
+  // $(".description").val(localStorage.getItem("nine"));
+  $(".description").each(function (i, element) {
+    console.log($(this).attr("id"), $(element).attr("data-hour"));
+    if ($(this).attr("id") === $(element).attr("data-hour")) {
+      var key = i.attr("data-hour");
+      var value = i.val();
+      console.log(key, value);
+      localStorage.setItem(key, value);
+    }
+  });
+  // textToSave = $(".description").val();
+  // console.log(textToSave);
+  // localStorage.setItem($(this).attr('id'), textToSave);
 });
+
+// $(this).attr('id')
+//using this to dynamically grab id and data hour
