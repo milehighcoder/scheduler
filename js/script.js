547b3ca07911e90e6a4d4e8f39887d8b4ea1e235
mykereel <mgmartnz@icloud.com>
2020-10-25 11:52:02 -0600

    added multiple time blocks

--- js/script.js
+++ js/script.js
@@ -1,25 +1,186 @@
 const m = moment().format("[Today's date is] dddd, MMMM Do.");
-
 // displays current day in viewport
 $("#currentDay").html(m); //JS = document.getElementById('currentDay').innerHTML = m;
 
 // 9:00 AM time block
-$("#nine").click(function () {
+$("#nine").click(function (event) {
+  //prevents the browser from auto refreshing
   event.preventDefault();
-  // $(".description").val(localStorage.getItem("nine"));
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
   $(".description").each(function (i, element) {
-    console.log($(this).attr("id"), $(element).attr("data-hour"));
-    if ($(this).attr("id") === $(element).attr("data-hour")) {
-      var key = i.attr("data-hour");
-      var value = i.val();
-      console.log(key, value);
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
       localStorage.setItem(key, value);
     }
   });
-  // textToSave = $(".description").val();
-  // console.log(textToSave);
-  // localStorage.setItem($(this).attr('id'), textToSave);
 });
 
-// $(this).attr('id')
-//using this to dynamically grab id and data hour
+$("#ten").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#eleven").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#twelve").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#one").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#two").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#three").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#four").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+$("#five").click(function (event) {
+  //prevents the browser from auto refreshing
+  event.preventDefault();
+  //this defines the id of the button that the user clicked
+  var button = $(this).attr("id");
+  //this loops through each HTML element that has a class of description
+  $(".description").each(function (i, element) {
+    //this checks to see if the button's id matches the textarea's data-hour
+    if (button === $(element).attr("data-hour")) {
+      //when we find that value that matches we set the hour as the key
+      var key = $(element).attr("data-hour");
+      // and we set the text of the textarea as the value
+      var value = $(element).val();
+      //this will set the key,value pair in local storage
+      localStorage.setItem(key, value);
+    }
+  });
+});
+
+//this will get the hour nine from local storage if it exists
+$("#nineInputSave").val(localStorage.getItem("nine"));
+$("#tenInputSave").val(localStorage.getItem("ten"));
+$("#elevenInputSave").val(localStorage.getItem("eleven"));
+$("#twelveInputSave").val(localStorage.getItem("twelve"));
+$("#oneInputSave").val(localStorage.getItem("one"));
+$("#twoInputSave").val(localStorage.getItem("two"));
+$("#threeInputSave").val(localStorage.getItem("three"));
+$("#fourInputSave").val(localStorage.getItem("four"));
+$("#fiveInputSave").val(localStorage.getItem("five"));
